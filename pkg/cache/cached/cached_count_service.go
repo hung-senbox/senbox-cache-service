8c7f32129dda5a37a08bdb2f91fef87f78fcfee2
hung-senbox <mr.hung@senbox.org>
2026-06-11 18:00:47 +0700

    add:GetMediaPortalCountByStudentIds

--- pkg/cache/cached/cached_count_service.go
+++ pkg/cache/cached/cached_count_service.go
@@ -10,6 +10,7 @@ import (
 
 type CachedCountService interface {
 	GetMediaPortalCountByStudentID(ctx context.Context, studentID string) ([]media.CountMediaStudentPortal, error)
+	GetMediaPortalCountByStudentIds(ctx context.Context, studentIDs []string) (map[string][]media.CountMediaStudentPortal, error)
 }
 
 type cachedCountService struct {
@@ -31,6 +32,35 @@ func (c *cachedCountService) GetMediaPortalCountByStudentID(ctx context.Context,
 	var result []media.CountMediaStudentPortal
 	if err := c.cache.Get(ctx, keys.GetCountCacheKey(studentID), &result); err != nil {
 		return nil, err
+	}
+
+	if len(result) == 0 {
+		return nil, nil
+	}
+
+	return result, nil
+}
+
+func (c *cachedCountService) GetMediaPortalCountByStudentIds(ctx context.Context, studentIDs []string) (map[string][]media.CountMediaStudentPortal, error) {
+	if len(studentIDs) == 0 {
+		return nil, nil
+	}
+
+	result := make(map[string][]media.CountMediaStudentPortal, len(studentIDs))
+	for _, studentID := range studentIDs {
+		if studentID == "" {
+			continue
+		}
+
+		studentPortalCounts, err := c.GetMediaPortalCountByStudentID(ctx, studentID)
+		if err != nil {
+			return nil, err
+		}
+		if len(studentPortalCounts) == 0 {
+			continue
+		}
+
+		result[studentID] = studentPortalCounts
 	}
 
 	if len(result) == 0 {
