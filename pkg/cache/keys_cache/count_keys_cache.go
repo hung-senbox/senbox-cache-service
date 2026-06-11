3ca287bfa50fe9eb6becfb2626051ee9a0f0ef1f
hung-senbox <mr.hung@senbox.org>
2026-06-11 16:00:06 +0700

    fix: cache key

--- pkg/cache/keys_cache/count_keys_cache.go
+++ pkg/cache/keys_cache/count_keys_cache.go
@@ -4,5 +4,5 @@ import "github.com/hung-senbox/senbox-cache-service/helper"
 
 // media
 func GetCountCacheKey(studentID string) string {
-	return string(helper.CountServicePrefix) + string(helper.MediaPrefix) + string(helper.MediaCountSubTypePortalByStudentID) + studentID
+	return string(helper.CountServicePrefix) + string(helper.MediaPrefix) + string(helper.MediaCountSubTypePortalByStudentID) + ":" + studentID
 }
