0d7a4cf476ad89c0f7745d5524406a5b0bc5cf19
hung-senbox <mr.hung@senbox.org>
2026-09-13 09:16:57 +0700

    add: GetAllLeaderAndStaffsByOrganizationID

--- pkg/cache/cached/cached_department_service.go
+++ pkg/cache/cached/cached_department_service.go
@@ -10,6 +10,7 @@ import (
 
 type CachedDepartmentService interface {
 	GetHierarchyDepartmentCacheKey(ctx context.Context, organizationID string) ([]*department.Department, error)
+	GetAllLeaderAndStaffsByOrganizationID(ctx context.Context, organizationID string) ([]*department.Leader, []*department.Staff, error)
 }
 
 type cachedDepartmentService struct {
@@ -38,4 +39,55 @@ func (c *cachedDepartmentService) GetHierarchyDepartmentCacheKey(ctx context.Con
 	}
 
 	return result, nil
+}
+
+func (c *cachedDepartmentService) GetAllLeaderAndStaffsByOrganizationID(ctx context.Context, organizationID string) ([]*department.Leader, []*department.Staff, error) {
+	if organizationID == "" {
+		return nil, nil, nil
+	}
+
+	departments, err := c.GetHierarchyDepartmentCacheKey(ctx, organizationID)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(departments) == 0 {
+		return nil, nil, nil
+	}
+
+	leaders := make([]*department.Leader, 0)
+	staffs := make([]*department.Staff, 0)
+	seenLeaders := make(map[string]struct{})
+	seenStaffs := make(map[string]struct{})
+
+	for _, dept := range departments {
+		if dept == nil {
+			continue
+		}
+
+		if dept.Leader.OwnerID != "" {
+			if _, ok := seenLeaders[dept.Leader.OwnerID]; !ok {
+				seenLeaders[dept.Leader.OwnerID] = struct{}{}
+				leader := dept.Leader
+				leaders = append(leaders, &leader)
+			}
+		}
+
+		for i := range dept.Staffs {
+			staff := dept.Staffs[i]
+			if staff.OwnerID == "" {
+				continue
+			}
+			if _, ok := seenStaffs[staff.OwnerID]; ok {
+				continue
+			}
+			seenStaffs[staff.OwnerID] = struct{}{}
+			staffs = append(staffs, &staff)
+		}
+	}
+
+	if len(leaders) == 0 && len(staffs) == 0 {
+		return nil, nil, nil
+	}
+
+	return leaders, staffs, nil
 }
